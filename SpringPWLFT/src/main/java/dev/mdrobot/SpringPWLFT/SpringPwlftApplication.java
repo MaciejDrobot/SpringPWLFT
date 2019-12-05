393d0457b8a5636fef9e855c2a9ac8d5f9bc6cab
Maciej Drobot <maciejdrobot@gmail.com>
2019-12-05 21:54:09 +0000

    initial commit

---
+++ SpringPWLFT/src/main/java/dev/mdrobot/SpringPWLFT/SpringPwlftApplication.java
@@ -0,0 +1,13 @@
+package dev.mdrobot.SpringPWLFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPwlftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPwlftApplication.class, args);
+	}
+
+}
---
+++ SpringPWLFT/src/test/java/dev/mdrobot/SpringPWLFT/SpringPwlftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mdrobot.SpringPWLFT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPwlftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
